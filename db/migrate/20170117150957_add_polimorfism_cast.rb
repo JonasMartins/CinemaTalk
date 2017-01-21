89724395fe159769b856aacbb9be45674fcc414e
JonasMartins <jonas.s.martins@hotmail.com>
2017-01-20 22:54:51 -0300

    Try migrate error

--- db/migrate/20170117150957_add_polimorfism_cast.rb
+++ db/migrate/20170117150957_add_polimorfism_cast.rb
@@ -1,7 +1,5 @@
 class AddPolimorfismCast < ActiveRecord::Migration
   def change
-  	drop_table :screewnriters
-
   	add_column :stars, :also_director, :boolean, default: false
   	add_column :stars, :also_screenwriter, :boolean, default: false
   	add_column :directors, :also_star, :boolean, default: false
