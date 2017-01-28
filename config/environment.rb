381895a13e590fc5facd0ba2e53199a00add95a9
JonasMartins <jonas.s.martins@hotmail.com>
2017-01-28 14:20:31 -0300

    Message error replaced by modal

--- config/environment.rb
+++ config/environment.rb
@@ -6,3 +6,8 @@ Rails.application.initialize!
 
 # Resolvendo problema do Uploader
 require 'carrierwave/orm/activerecord'
+
+# acabando com field_with_erros
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  html_tag.html_safe
+end
